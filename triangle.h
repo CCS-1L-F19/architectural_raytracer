038f8a601507ea3600a23cf58b5505e820e5438e
hari387 <narahari_rao@ucsb.edu>
2019-11-13 12:19:57 -0800

    triangleh written

---
+++ triangle.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "glm/glm.hpp"
+#include "hitable.h"
+#include "material.h"
+#include "ray.h"
+
+using namespace glm;
+
+class triangle : public hitable{
+
+private:
+	vec3 p1,p2,p3;
+	vec3 normal;
+	material* matptr;
+
+public:
+	triangle();
+	~triangle();
+	vec3 min_bound();
+	vec3 max_bound();
+	bool hit(ray & r,float t_min,float t_max,hit_record & rec) const;
+	bool in_box(vec3 min,vec3 max);
+	
+};
